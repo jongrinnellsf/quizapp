8605f157d72a161b28bd160004b264ac4f125e38
Jon Grinnell <jgrinnell0@gmail.com>
2019-07-17 20:21:08 -0700

    initial commit

---
+++ questionbank.js
@@ -0,0 +1,122 @@
+const QUESTIONBANK = [ 
+    {
+      question: 'What is ethereum?',
+      answers: [
+        'A turing complete AI',
+        'A cryptocurrency with a fixed price',
+        'A programming language',
+        'An open software platform built on blockchain technology'
+        ],
+      correctAnswer: 'An open software platform built on blockchain technology',
+      icon: '',
+      alt: ''
+    },
+    {
+      question: 'Who is known as the creater of ethereum?',
+      answers: [
+        'Satoshi Nakamoto',
+        'Vitalik Buterin',
+        'David Chaum',
+        'Craig Wright'
+        ],
+        correctAnswer: 'Vitalik Buterin',
+        icon: '',
+        alt: ''
+    },
+    {
+      question: 'Which year was the ethereum white paper released?',
+      answers: [
+        '2013',
+        '2009',
+        '2014',
+        '2008'
+        ],
+        correctAnswer: '2014',
+        icon: '',
+        alt: ''
+    },
+    {
+      question: 'Which option best describes how ethereum might be used?',
+      answers: [
+        'As a computing platform for decentralized applications',
+        'For validating bitcoin blocks',
+        'For verifying transations, specifically for banks',
+        'For buying lambos'
+        ],
+      correctAnswer: 'As a computing platform for decentralized applications',
+      icon: '',
+      alt: ''
+    },
+    {
+      question: 'What is an ethereum smart contract?',
+      answers: [
+        'An agreement between two bitcoin nodes',
+        'An agreement between ethereum users and ethereum lawyers',
+        'A digital agreement that is programmed to execute only if certain conditions are met',
+        'An quantum computer running on solar and wind'
+        ],
+      correctAnswer: 'A digital agreement that is programmed to execute only if certain conditions are met',
+      icon: '',
+      alt: ''
+    },
+    {
+      question: 'What is an ethereum token?',
+      answers: [
+        'A cryptocurrency which always appreciates over time',
+        'A digital asset built on top of ethereum',
+        'A digital asset for investment only',
+        'A J.R.R Tolkien collectable'
+        ],
+      correctAnswer: 'A digital asset built on top of ethereum',
+      icon: '',
+      alt: ''
+    },
+    {
+      question: 'What is the name of ethereum\'s annual conference?',
+      answers: [
+      'Ethcon',
+      'Devcon',
+      'DevLearn',
+      'The Puppy Bowl'
+      ],
+      correctAnswer: 'Devcon',
+      icon: '',
+      alt: ''
+    },
+    {
+      question: 'Which option best describes how bitcoin and ethereum are different?',
+      answers: [
+        'Bitcoin is anonymous, but ethereum requires one\'s identity for use',
+        'There is no difference',
+        'Bitcoin miners earn ETH, while ethereum miners earn BTC',
+        'Bitcoin is a peer-to-peer electronic cash system, Ethereum allows for scripts of code to be run as a global computational network'
+      ],
+      correctAnswer: 'Bitcoin is a peer-to-peer electronic cash system, Ethereum allows for scripts of code to be run as a global computational network',
+      icon: '',
+      alt: ''
+    },
+    {
+      question: 'Where can you store your cryptocurrency, such as ETH?',
+      answers: [
+      'On a floppy disk',
+      'In a digital wallet',
+      'Typically always on a microchip embedded within your credit card',
+      'In a piggy bank'
+      ],
+      correctAnswer: 'In a digital wallet',
+      icon: '',
+      alt: ''
+    },
+    {
+      question: 'What is the best way to learn more about ethereum?',
+      answers: [
+        'Visit ethereum.org',
+        'Follow the ethereum community on \'crypto\' Twitter',
+        'Find a local ethereum meetup or engage in a local Ethereum hackathon',
+        'All of the above!'
+    ],
+    correctAnswer: 'All of the above!',
+    icon: '',
+    alt: ''
+    }
+];
